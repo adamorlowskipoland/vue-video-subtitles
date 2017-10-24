7d891b0b257d25c0f90f3f84160bea642e966250
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-10-25 00:17:09 +0200

    create project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <div class="row text-center">
+      <div class="columns small-6">
+        <div>
+          <p>
+            start
+          </p>
+        </div>
+      </div>
+      <div class="columns small-6">
+        <div>
+          <p>
+            end
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+  div {
+    /*background: lightskyblue;*/
+    p {
+      background: lightskyblue;
+      color: tomato;
+    }
+  }
+</style>
