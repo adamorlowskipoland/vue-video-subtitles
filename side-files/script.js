a0ae955a28370f9fec01fd9f19149bf7146e4326
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-10-26 00:31:41 +0200

    add side files - tried to do it as simple as i can at first

---
+++ side-files/script.js
@@ -0,0 +1,64 @@
+const lines = [
+  {
+    min: 0,
+    max: 1,
+    text: "Linia nr 1"
+  },
+  {
+    min: 5,
+    max: 6,
+    text: "Llinia nr 2"
+  },
+  {
+    min: 8,
+    max: 9,
+    text: "Llinia nr 3"
+  },
+  {
+    min: 11,
+    max: 14,
+    text: "Llinia nr 4"
+  }
+];
+
+const videoWrapper = document.querySelector('.video-wrapper')
+const videoText = document.querySelector('.video-wrapper__text')
+const video = document.getElementById('video')
+
+let currentTime
+let currentData = {}
+
+// jeżeli current time jest inbetween current data return
+// jeżeli nie jest ustaw nowe currentdata
+
+function setCurrentTime() {
+  currentTime = Math.floor(video.currentTime)
+  displayLine()
+}
+
+function displayLine() {
+  if (currentData) {
+    if (inBetween(currentTime, currentData)) {
+      return
+    } else {
+      setCurrentData()
+    }
+  } else {
+    setCurrentData()
+  }
+}
+
+function setCurrentData () {
+  videoText.textContent = ""
+  currentData = lines.find(line => {
+    if (inBetween(currentTime, line)) {
+      videoText.textContent = line.text
+    }
+  })
+}
+
+function inBetween(x, {min, max}) {
+  return x >= min && x <= max;
+}
+
+video.addEventListener('timeupdate', setCurrentTime)
